092169727e6d646851a5c723f2de916d8fcb28f4
Guite <info@guite.de>
2019-11-28 09:08:51 +0100

    trigger rebuild

--- .github/scripts/perform-generation.sh
+++ .github/scripts/perform-generation.sh
@@ -6,12 +6,12 @@ cd work
 wget https://updates.modulestudio.de/standalone/ModuleStudio-generator.jar
 
 echo "Fetch model and regenerate it"
-cp ../src/modules/$MODULE_NAME/Resources/docs/model/$MODEL_NAME ./$MODEL_NAME
+cp ../modules/$MODULE_NAME/Resources/docs/model/$MODEL_NAME ./$MODEL_NAME
 java -jar ModuleStudio-generator.jar $MODEL_NAME output
 
 echo "Remove unrequired files and copy new module into the checkout"
 outputPath="output/$MODULE_NAME"
 rm $outputPath/Resources/public/images/*.png
 find $outputPath -type f -name '*.generated.*' -delete
-cp -R $outputPath/* ../src/modules/$MODULE_NAME
+cp -R $outputPath/* ../modules/$MODULE_NAME
 cd ..
